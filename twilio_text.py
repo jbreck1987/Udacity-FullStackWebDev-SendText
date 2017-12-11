f18f620bcfde4017c6890d45fd11141354d2c352
Josh <jbreck1987@users.noreply.github.com>
2017-12-11 18:47:24 -0500

    short script that uses Twilio service to send texts

---
+++ twilio_text.py
@@ -0,0 +1,20 @@
+from twilio.rest import Client
+import time
+
+auth_token = '034050e5dfdd7a77ad5adeb91f3dcc77'
+acct_SID = 'AC68700b29508ba3e1f3e7c06c6ae52e02'
+
+print('Sending a text...')
+time.sleep(3)
+
+newClient = Client(username=acct_SID, password=auth_token)
+new_message = newClient.messages.create(
+    to="+15137800683",
+    from_="+15132823803",
+    body="Yo, this is an app texting your phone!"
+)
+
+new_message_data = newClient.messages(new_message.sid).fetch()
+
+print('Status = {}, Direction = {}'.format(
+    new_message_data.status, new_message_data.direction))
